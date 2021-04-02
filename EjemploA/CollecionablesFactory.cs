7727e07f2999f503a50df405f5010cfc52aefc7e
Maciej Nalepa <maciej.nalepa@alu.uclm.es>
2021-04-02 17:19:46 +0200

    Moved images to media

---
+++ EjemploA/CollecionablesFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace EjemploA
+{
+    class CollecionablesFactory
+    {
+        private static ImageSource[] IMAGES;
+        private StackPanel mPanel;
+
+        public CollecionablesFactory(StackPanel target)
+        {
+            mPanel = target;
+        }
+
+        private Image createImageElement(ImageSource src)
+        {
+            Image elem = new Image();
+            elem.Source = src;
+            return elem;
+        }
+
+        private Image createImageElement(ImageSource src, MouseButtonEventHandler clickEvent)
+        {
+            Image elem = createImageElement(src);
+            elem.MouseLeftButtonDown += clickEvent;
+            return elem;
+        }
+
+        public void addRandomCollecionable()
+        {
+            mPanel.Children.Add(createImageElement());
+        }
+    }
+}
