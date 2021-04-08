85211481f6fa32668bd0cc5529233f9b5e1b2776
Maciej Nalepa <maciej.nalepa@alu.uclm.es>
2021-04-08 10:53:03 +0200

    Persistence serializabe classes

---
+++ Achievement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Tamagotchi
+{
+    [Serializable]
+    class Achievement
+    {
+        private string mDescription;
+        private string mSource;
+
+        public string Source { get => mSource; }
+        public string Descritpion { get => mDescription; }
+
+        public Achievement(string src, string description)
+        {
+            mDescription = description;
+            mSource = src;
+        }
+
+        public Image GetImage()
+        {
+            Image img = new Image();
+            img.Stretch = Stretch.Uniform;
+            img.HorizontalAlignment = HorizontalAlignment.Center;
+            img.VerticalAlignment = VerticalAlignment.Center;
+            img.Source = GetImageFromUri(Source);
+            img.ToolTip = Descritpion;
+            return img;
+        }
+
+        private ImageSource GetImageFromUri(string uri)
+        {
+            BitmapImage img = new BitmapImage();
+            img.BeginInit();
+            img.UriSource = new Uri(uri, UriKind.Relative);
+            img.EndInit();
+            return img;
+        }
+    }
+}
--- PlayerData.cs
+++ PlayerData.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Tamagotchi
 {
+    [Serializable]
     class PlayerData
     {
         private string mName;
